ac2bbb957eea2fdb532c35ee63547761d7466a46
Jonathan Carvalho <jonathan.carvalho1807@gmail.com>
2021-07-24 19:45:51 -0300

    Remove sticky nav and make side nav responsive

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const nav = document.getElementById("nav-submenu");
-const sticky = nav.offsetTop;
 
-console.log(sticky);
+const overlay = document.getElementById("overlay");
 
-document.addEventListener("scroll", () => {
-    if(window.pageYOffset > sticky){
-        nav.classList.add("sticky");
-    }else{
-        nav.classList.remove("sticky");
-    }
-});
+const openNav = document.getElementById("bars");
+
+const closeNav = document.getElementById("close");
+
+openNav.addEventListener("click", () => {
+    nav.classList.add("active");
+    overlay.classList.add("active");
+})
+
+closeNav.addEventListener("click", () => {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
+})
